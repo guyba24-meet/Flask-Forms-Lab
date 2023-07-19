d32cb16bc90fbb2b36133f69e5007e55bb1b869a
guyba24-meet <139680985+guyba24-meet@users.noreply.github.com>
2023-07-19 14:02:43 +0300

    thank you Jihad

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -6,19 +6,34 @@ app = Flask(  # Create a flask app
 	template_folder='templates',  # Name of html file folder
 	static_folder='static'  # Name of directory for static files
 )
+app.config['SECRET_KEY']='super-secret-key'
 
-
-username = "llo2ay"
+username = "Guy"
 password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+facebook_friends=["Itay","Ido","Eyal", "Yair", "Shachar", "Noy", "Jihad"]
 
-
-@app.route('/')  # '/' for the default page
+@app.route('/', methods=['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if request.method == 'POST':
+		if request.form['username'] == username and request.form['password'] == password:
+			return redirect(url_for('homepage'))
+		else:
+			return render_template('login.html')
+	else:
+		return render_template('login.html')
 
+@app.route('/home')
+def homepage():
+	return render_template('home.html',
+	 facebook_friends=facebook_friends
+)
 
+@app.route('/friends_exists/<string:friend>')
+def friendsExists(friend):
+	if friend in facebook_friends:
+		return render_template('friend_exists.html', f = True)
+	else:
+		return render_template('friend_exists.html', f = False)
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
